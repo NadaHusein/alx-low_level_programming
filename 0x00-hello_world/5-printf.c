01cf121800213bf5652dde7a38d4fe13591ffecf
Nada Husein <nada.hussein796@gmail.com>
2023-08-10 15:05:02 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 # include<stdio.h>
 int main(){
 /*Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.*/
-	printf("with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return(0);
 }
