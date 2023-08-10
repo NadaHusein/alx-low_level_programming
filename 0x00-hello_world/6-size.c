8086e1840fb3a420b10b9f455fb294e51d9201e0
Nada Husein <nada.hussein796@gmail.com>
2023-08-10 15:11:23 -0700

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 # include<stdio.h>
-int main(void)
 /*Write a C program that prints the size of various types on the computer it is compiled and run on.*/
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
