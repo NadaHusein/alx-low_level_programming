753518c4b857d0681f9bedde5af940920c3bb479
Nada Husein <nada.hussein796@gmail.com>
2023-08-17 07:45:00 -0700

    advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,44 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * times_table - prints the 9 times table
- *
- * Return: void
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
 void print_times_table(int n)
 {
-	int row, column, product, tens, ones;
-	if (n <= 15 && n >= 0){
-		for (row = 0; row <= n; row++)
+	int num, mult, prod;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
 		{
-			for (column = 0; column <= n; column++)
+			_putchar('0');
+
+			for (mult = 1; mult <= n; mult++)
 			{
-				product = row * column;
-				tens = product / 10;
-				ones = product % 10;
-				if (column == 0)
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
 				{
-					_putchar('0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				else if (product < 10)
+				else if (prod <= 99 && prod >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(ones + '0');
+					_putchar((prod / 10) + '0');
 				}
-				else if (product < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(tens + '0');
-					_putchar(ones + '0');
-				}
-				else
-				{
-					_putchar(',');
-                                	_putchar(' ');
-                                	_putchar(tens + '0');
-                                	_putchar(ones + '0');
-				}
-				 putchar('\n');
+				_putchar((prod % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
