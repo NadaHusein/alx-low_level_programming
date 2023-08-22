751bbc6d184fcdb3399f4995aa4b78e4f603dbbf
Nada Husein <nada.hussein796@gmail.com>
2023-08-22 02:36:54 -0700

    first task update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,12 @@
-
-/*
- * Write a function that takes a pointer to an int
- *  as parameter and updates 
- *  the value it points to to 98.
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ *               and updates the value it points to 98
+ *
+ * @n: int parameter
+ *
+ * Return: Nothing
  */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
