3b490d2a847e51b27dd38b198151189bfd15bc6e
Nada Husein <nada.hussein796@gmail.com>
2023-08-22 02:34:33 -0700

    first task update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,10 @@
-#include "main.h"
 
 /*
  * Write a function that takes a pointer to an int
  *  as parameter and updates 
  *  the value it points to to 98.
  */
-
-int main(void)
+void reset_to_98(int *n)
 {
-	int n;
+	*n = 98;
 }
