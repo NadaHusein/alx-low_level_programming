5ddcc5c1debcf078d5804a60736db41125867c58
Nada Husein <nada.hussein796@gmail.com>
2023-09-03 04:08:03 -0700

    3rd task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+// C++ program to implement
+// command-lien arguments
+#include <stdio.h>
+ 
+// Command Line Arg
+void main(int argc, char *argv[])   
+{
+    int i;
+    for (i = 0; i < argc; i++)
+    {
+        // Printing all the Arguments
+        printf("%s ", argv[i]);       
+    }
+    printf("\n");
+}
