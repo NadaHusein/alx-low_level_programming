8c19dfa5af4bec67bb8a6f75a7c8d3655800a613
NadaHusein <nada.hussein796@gmail.com>
2023-10-03 05:49:24 -0700

    updates

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,6 +16,13 @@ int _strlen(char *s)
                 i++;
         return (i);
 }
+/**
+ * append_text_to_file - appends a text at the end of a file.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
